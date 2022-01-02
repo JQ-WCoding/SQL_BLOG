ba4ae1fb1d349477a671cb8deb87f2f633261473
Jun_Q <junlee36520620@gmail.com>
2022-01-02 13:44:20 +0900

    변수 쿼리

--- src/sql/sql_procedure/procedure.sql
+++ src/sql/sql_procedure/procedure.sql
@@ -1,16 +1,22 @@
 /* 프로시저 생성 */
 CREATE PROCEDURE dbo.[TEST_PROC](
-    /* 입력받는 변수 */
-    @I_INPUT VARCHAR(10),
-    /* 반환하는 변수 */
-    @O_OUTPUT VARCHAR(10) OUT
+    /* 전역변수 */
+    @I_INPUT VARCHAR(10), /* 입력받는 변수 */
+    @O_OUTPUT VARCHAR(10) OUT /* 반환하는 변수 */
 )
 AS
 BEGIN
-    /* 지역변수 선언 */
+    /* 지역변수 */
     DECLARE @v_local VARCHAR(20)
 
-    /* 내부 블록 */
+    /* 지역변수 선언 및 초기화 */
+    DECLARE @v_local_set INT = 10
+
+    SET @I_INPUT = 10
+    SET @O_OUTPUT = 20
+    SELECT @v_local, @v_local_set /*  */
+
+    /*  */
     IF (@I_INPUT < 10)
         BEGIN
             DECLARE @v_inner_local VARCHAR(10)
@@ -23,13 +29,5 @@ BEGIN
         BEGIN
             /* SELECT @v_inner_local */
         END
-
     /* SELECT @v_inner_local 에러가 발생함 */
-
-    /* 지역변수 선언 및 초기화 */
-    DECLARE @v_local_set INT = 10
-
-    SET @I_INPUT = 10
-    SET @O_OUTPUT = 20
-    SELECT @v_local, @v_local_set
 END
