acb2807ec84e9f1c3094d2c9ffd827a0f2eaee66
Jun_Q <junlee36520620@gmail.com>
2022-01-04 00:21:30 +0900

    변수 쿼리

--- src/sql/sql_procedure/procedure.sql
+++ src/sql/sql_procedure/procedure.sql
@@ -2,7 +2,8 @@
 CREATE PROCEDURE dbo.[TEST_PROC](
     /* 전역변수 */
     @I_INPUT VARCHAR(10), /* 입력받는 변수 */
-    @O_OUTPUT VARCHAR(10) OUT /* 반환하는 변수 */
+    @O_OUTPUT VARCHAR(10) OUTPUT, /* 반환하는 변수(OUT 과 동일) */
+    @PARAMETER VARCHAR(10) READONLY /* READONLY 읽기만 가능 (프로시저 내에서 변수 초기화 불가능) */
 )
 AS
 BEGIN
@@ -12,12 +13,16 @@ BEGIN
     /* 지역변수 선언 및 초기화 */
     DECLARE @v_local_set INT = 10
 
-    SET @I_INPUT = 10
-    SET @O_OUTPUT = 20
-    SELECT @v_local, @v_local_set /*  */
+/*    SET @I_INPUT = 10
+    SET @O_OUTPUT = 20*/
+    SELECT @v_local, @v_local_set
 
-    /*  */
-    IF (@I_INPUT < 10)
+    SET @PARAMETER = 10
+    SELECT @PARAMETER
+
+    /* 지역변수 범위 확인하기 */
+    IF (@v_local_set < 10)
+        /* BEGIN ~ END 한 블록 */
         BEGIN
             DECLARE @v_inner_local VARCHAR(10)
 
