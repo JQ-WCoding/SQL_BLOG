bf8a844b86ae0dd7a4980d3a245e40654c405161
Jun_Q <junlee36520620@gmail.com>
2022-01-05 01:05:21 +0900

    프로시저 생성 및 호출

--- src/sql/sql_procedure/procedure.sql
+++ src/sql/sql_procedure/procedure.sql
@@ -2,23 +2,17 @@
 CREATE PROCEDURE dbo.[TEST_PROC](
     /* 전역변수 */
     @I_INPUT VARCHAR(10), /* 입력받는 변수 */
-    @O_OUTPUT VARCHAR(10) OUTPUT, /* 반환하는 변수(OUT 과 동일) */
-    @PARAMETER VARCHAR(10) READONLY /* READONLY 읽기만 가능 (프로시저 내에서 변수 초기화 불가능) */
+    @O_OUTPUT VARCHAR(10) OUTPUT /* 반환하는 변수(OUT 과 동일) */
 )
 AS
 BEGIN
-    /* 지역변수 */
+    /* 지역변수 선언 */
     DECLARE @v_local VARCHAR(20)
 
     /* 지역변수 선언 및 초기화 */
     DECLARE @v_local_set INT = 10
 
-/*    SET @I_INPUT = 10
-    SET @O_OUTPUT = 20*/
     SELECT @v_local, @v_local_set
-
-    SET @PARAMETER = 10
-    SELECT @PARAMETER
 
     /* 지역변수 범위 확인하기 */
     IF (@v_local_set < 10)
@@ -36,3 +30,6 @@ BEGIN
         END
     /* SELECT @v_inner_local 에러가 발생함 */
 END
+
+/* 호출과 파라미터 입력 예시 */
+EXEC TEST_PROC '10'
