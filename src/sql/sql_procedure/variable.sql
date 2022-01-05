a0a1dd540a3b9c1b68711dd4598f79f85957973d
Jun_Q <junlee36520620@gmail.com>
2022-01-06 00:34:04 +0900

    수정

--- src/sql/sql_procedure/variable.sql
+++ src/sql/sql_procedure/variable.sql
@@ -7,7 +7,9 @@ BEGIN
     /* 지역변수 */
     DECLARE @local INT = 1
 
-    /* 변수 @local 값이 10이하이면 @local 값을 조회한다 */
+    /*
+        변수 @local 값이 10이하이면 @local 값을 조회한다
+    */
     IF (@local < 10)
         BEGIN
             SELECT @local
@@ -18,7 +20,7 @@ BEGIN
     /*
         변수 @local 값이 10까지 반복한다
     */
-    WHILE (@local = 10)
+    WHILE (@local <= 10)
         BEGIN
             /* while 내부 지역변수 */
             DECLARE @addNum INT = 1
@@ -34,6 +36,5 @@ BEGIN
 
     /* 전역변수 */
     SELECT @@ERROR -- 마지막 오류번호
-
     SELECT @@IDENTITY -- 마지막 IDENTITY 값
 END
