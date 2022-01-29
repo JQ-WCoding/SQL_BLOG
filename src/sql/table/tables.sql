2275de0e7b7fafb282f5e14531a76fd2f42d431e
Jun_Q <junlee36520620@gmail.com>
2022-01-29 22:28:13 +0900

    임시 테이블과 테이블 변수

--- src/sql/table/tables.sql
+++ src/sql/table/tables.sql
@@ -2,36 +2,111 @@
 임시 테이블과 테이블 변수의 차이점 구분
 */
 
-BEGIN TRAN
 BEGIN
-    DECLARE @TEMP_TABLE TABLE
-                        (
-                            ID       VARCHAR(10),
-                            NAME     VARCHAR(10),
-                            PASSWORD VARCHAR(10)
-                        )
+    BEGIN TRANSACTION
+        DECLARE @cnt INT = 1
 
-    INSERT INTO @TEMP_TABLE(ID, NAME, PASSWORD)
-    VALUES ('AAA', 'LEE', '1234')
+        DECLARE @TEMP_TABLE TABLE
+                            (
+                                NO           INT IDENTITY (1,1) NOT NULL,
+                                INS_DATETIME DATETIME           NOT NULL
+                            )
 
-    CREATE TABLE ##TEMP_TABLE
-    (
-        ID       VARCHAR(10),
-        NAME     VARCHAR(10),
-        PASSWORD VARCHAR(10)
-    )
+        WHILE (@cnt < 100)
+            BEGIN
+                INSERT INTO @TEMP_TABLE(INS_DATETIME)
+                VALUES (GETDATE())
 
-    INSERT INTO ##TEMP_TABLE(ID, NAME, PASSWORD)
-    VALUES ('AAA', 'LEE', '1234')
+                SET @cnt += 1
+            END
 
-
-    SELECT *
-    FROM @TEMP_TABLE
-
-    SELECT *
-    FROM ##TEMP_TABLE
-
-    DROP TABLE ##TEMP_TABLE
+        /* 테이블 변수 */
+        SELECT *
+        FROM @TEMP_TABLE
+        ORDER BY INS_DATETIME DESC
+    ROLLBACK TRANSACTION
 END
-ROLLBACK TRAN
 
+
+BEGIN
+    BEGIN TRANSACTION
+        DECLARE @cnt INT = 1
+
+        CREATE TABLE #TEMP_TABLE
+        (
+            NO           INT IDENTITY (1,1) NOT NULL,
+            INS_DATETIME DATETIME           NOT NULL
+        )
+
+        WHILE (@cnt < 100)
+            BEGIN
+                INSERT INTO #TEMP_TABLE(INS_DATETIME)
+                VALUES (GETDATE())
+
+                SET @cnt += 1
+            END
+
+        /* 임시 테이블 */
+        SELECT *
+        FROM #TEMP_TABLE
+        ORDER BY INS_DATETIME DESC
+
+        DROP TABLE #TEMP_TABLE
+    ROLLBACK TRANSACTION
+END
+
+
+
+BEGIN
+    BEGIN TRANSACTION
+        DECLARE @cnt INT = 1
+
+        DECLARE @TEMP_TABLE TABLE
+                            (
+                                NO           INT IDENTITY (1,1) NOT NULL,
+                                INS_DATETIME DATETIME           NOT NULL
+                            )
+
+        WHILE (@cnt < 100000)
+            BEGIN
+                INSERT INTO @TEMP_TABLE(INS_DATETIME)
+                VALUES (GETDATE())
+
+                SET @cnt += 1
+            END
+
+        /* 테이블 변수 */
+        SELECT *
+        FROM @TEMP_TABLE
+        ORDER BY INS_DATETIME DESC
+    ROLLBACK TRANSACTION
+END
+
+
+
+BEGIN
+    BEGIN TRANSACTION
+        DECLARE @cnt INT = 1
+
+        CREATE TABLE #TEMP_TABLE
+        (
+            NO           INT IDENTITY (1,1) NOT NULL,
+            INS_DATETIME DATETIME           NOT NULL
+        )
+
+        WHILE (@cnt < 100000)
+            BEGIN
+                INSERT INTO #TEMP_TABLE(INS_DATETIME)
+                VALUES (GETDATE())
+
+                SET @cnt += 1
+            END
+
+        /* 임시 테이블 */
+        SELECT *
+        FROM #TEMP_TABLE
+        ORDER BY INS_DATETIME DESC
+
+        DROP TABLE #TEMP_TABLE
+    ROLLBACK TRANSACTION
+END
