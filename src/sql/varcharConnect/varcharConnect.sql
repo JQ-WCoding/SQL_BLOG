b27eb5dfc2640701af64cbe38d37caa7e27e673c
Jun_Q <junlee36520620@gmail.com>
2022-03-02 23:40:53 +0900

    VARCHAR 연결하기

--- src/sql/varcharConnect/varcharConnect.sql
+++ src/sql/varcharConnect/varcharConnect.sql
@@ -20,18 +20,19 @@ BEGIN
     FROM @TEMP
     WHERE SEQ = 1
 
+    -- 임의 데이터 넣기
     INSERT INTO @TEMP
     VALUES ( 2
            , '서울 종로구 '
            , '사직로 161 '
            , null)
 
-    -- 값이 합쳐지지 않고 null 이 나온다
+    -- 값이 합쳐지지 않고 null 이 나온다 (하나라도 값이 null인 경우 varchar를 합치면 전부 null이 된다.)
     SELECT ADDR1 + ADDR2 + ADDR3 AS ADDR
     FROM @TEMP
     WHERE SEQ = 2
 
-    -- ISNULL 을 통해 null인 경우 '' 을 이용해 합친다
+    -- ISNULL 을 통해 null인 경우 ''인 공란으로 변환시켜 varchar 형을 유지하면 varchar 합치기가 가능해진다
     SELECT ISNULL(ADDR1, '') + ISNULL(ADDR2, '') + ISNULL(ADDR3, '') AS ADDR
     FROM @TEMP
     WHERE SEQ = 2
