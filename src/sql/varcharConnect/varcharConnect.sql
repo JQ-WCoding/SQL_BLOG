ec756dc0821313a4055b04ec1fae367885f4e6f4
Jun_Q <junlee36520620@gmail.com>
2022-03-03 00:00:36 +0900

    N 추가

--- src/sql/varcharConnect/varcharConnect.sql
+++ src/sql/varcharConnect/varcharConnect.sql
@@ -11,9 +11,9 @@ BEGIN
     -- 임의 데이터 넣기(광화문 주소)
     INSERT INTO @TEMP
     VALUES ( 1
-           , '서울 종로구 '
-           , '효자로 12 '
-           , '국립고궁박물관')
+           , N'서울 종로구 '
+           , N'효자로 12 '
+           , N'국립고궁박물관')
 
     -- SEQ 1의 주소 합치기
     SELECT ADDR1 + ADDR2 + ADDR3 AS ADDR
@@ -23,8 +23,8 @@ BEGIN
     -- 임의 데이터 넣기
     INSERT INTO @TEMP
     VALUES ( 2
-           , '서울 종로구 '
-           , '사직로 161 '
+           , N'서울 종로구 '
+           , N'사직로 161 '
            , null)
 
     -- 값이 합쳐지지 않고 null 이 나온다 (하나라도 값이 null인 경우 varchar를 합치면 전부 null이 된다.)
